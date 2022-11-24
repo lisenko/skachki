69d8e13b1dbed6f146458e35a051430b9488f129
lisenko <lix7@me.com>
2022-11-24 19:29:34 +0300

    схема бд

---
+++ skachki.sql
@@ -0,0 +1,46 @@
+CREATE TABLE `owners` (
+  `id` int PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `address` varchar(255),
+  `phone` varchar(32)
+);
+
+CREATE TABLE `horses` (
+  `id` int PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `owner` int NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `sex` varchar(10),
+  `age` int
+);
+
+CREATE TABLE `jockeys` (
+  `id` int PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `address` varchar(255),
+  `age` int,
+  `rating` int
+);
+
+CREATE TABLE `battles` (
+  `id` int PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `name` varchar(255),
+  `location` varchar(255) NOT NULL,
+  `startdate` datetime NOT NULL
+);
+
+CREATE TABLE `results` (
+  `battle` int NOT NULL,
+  `horse` int NOT NULL,
+  `jockey` int NOT NULL,
+  `place` int NOT NULL,
+  `time` time NOT NULL,
+  PRIMARY KEY (`battle`, `horse`)
+);
+
+ALTER TABLE `horses` ADD FOREIGN KEY (`owner`) REFERENCES `owners` (`id`);
+
+ALTER TABLE `results` ADD FOREIGN KEY (`battle`) REFERENCES `battles` (`id`);
+
+ALTER TABLE `results` ADD FOREIGN KEY (`horse`) REFERENCES `horses` (`id`);
+
+ALTER TABLE `results` ADD FOREIGN KEY (`jockey`) REFERENCES `jockeys` (`id`);
