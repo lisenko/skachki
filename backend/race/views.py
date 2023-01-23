82f409e110557e165343f0b1e9d9b518a6279714
lisenko <lix7@me.com>
2023-01-23 23:30:21 +0300

    отображение баттлов

--- backend/race/views.py
+++ backend/race/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from django.template import loader
 from django.http import HttpResponse
 
+from .models import owner, horse, jockey, battle, result
+
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    context = {
+        'battles': battle.objects.all().order_by('-startdate'),
+    }
+    return render(request, 'race/battles.html', context)
