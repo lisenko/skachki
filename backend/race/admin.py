1c8b0a1a9e941f012faa56041a8594f5753f6910
lisenko <lix7@me.com>
2023-01-24 05:14:32 +0300

    свистоперделки для админки

--- backend/race/admin.py
+++ backend/race/admin.py
@@ -1,8 +1,26 @@
 from django.contrib import admin
 from .models import owner, horse, jockey, battle, result
 
-admin.site.register(owner)
-admin.site.register(horse)
-admin.site.register(jockey)
-admin.site.register(battle)
-admin.site.register(result)
+@admin.register(owner)
+class ownerAdmin(admin.ModelAdmin):
+    list_display = ('name', 'address', 'phone')
+    search_fields = ('name', 'address', 'phone')
+@admin.register(horse)
+class horseAdmin(admin.ModelAdmin):
+    list_display = ('name', 'owner', 'sex', 'age')
+    search_fields = ('name', 'owner')
+    list_filter = ('sex',)
+@admin.register(jockey)
+class jockeyAdmin(admin.ModelAdmin):
+    list_display = ('name', 'address', 'age', 'rating')
+    search_fields = ('name', 'address')
+@admin.register(battle)
+class battleAdmin(admin.ModelAdmin):
+    list_display = ('name', 'location', 'startdate')
+    search_fields = ('name', 'location')
+    list_filter = ('location',)
+@admin.register(result)
+class resultAdmin(admin.ModelAdmin):
+    list_display = ('battle', 'horse', 'jockey', 'time')
+    search_fields = ('battle', 'horse', 'jockey')
+    list_filter = ('battle', 'horse', 'jockey',)
