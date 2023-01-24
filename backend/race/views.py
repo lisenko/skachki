6fcb0b962e5e4d4ff966c1523d3dc570c40a9b3e
lisenko <lix7@me.com>
2023-01-24 04:54:48 +0300

    базовый шаблон, ссылки на всех страницах

--- backend/race/views.py
+++ backend/race/views.py
@@ -5,12 +5,14 @@ from .models import owner, horse, jockey, battle, result
 
 def battleslist(request):
     context = {
+        'title': 'Состязания',
         'battles': battle.objects.all().order_by('-startdate'),
     }
     return render(request, 'race/battles.html', context)
 
 def resultslist(request, battle_id):
     context = {
+        'title': 'Результаты',
         'battle': battle.objects.get(id=battle_id),
         'results': result.objects.filter(battle=battle_id).order_by('time'),
     }
@@ -18,24 +20,28 @@ def resultslist(request, battle_id):
 
 def ownerslist(request):
     context = {
+        'title': 'Владельцы',
         'owners': owner.objects.all().order_by('name'),
     }
     return render(request, 'race/owners.html', context)
 
 def horseslist(request):
     context = {
+        'title': 'Лошади',
         'horses': horse.objects.all().order_by('name'),
     }
     return render(request, 'race/horses.html', context)
 
 def ratingslist(request):
     context = {
+        'title': 'Рейтинг',
         'jockeys': jockey.objects.all().order_by('-rating'),
     }
     return render(request, 'race/rating.html', context)
 
 def horsedetails(request, horse_id):
     context = {
+        'title': 'Лошадь',
         'horse': horse.objects.get(id=horse_id),
         'results_places': zip(result.objects.filter(horse=horse_id).order_by('-battle__startdate'), 
         [result.objects.filter(time__lt=r.time, battle=r.battle).count()+1 
@@ -45,6 +51,7 @@ def horsedetails(request, horse_id):
 
 def jockeydetails(request, jockey_id):
     context = {
+        'title': 'Жокей',
         'jockey': jockey.objects.get(id=jockey_id),
         'results_places': zip(result.objects.filter(jockey=jockey_id).order_by('-battle__startdate'), 
         [result.objects.filter(time__lt=r.time, battle=r.battle).count()+1 
